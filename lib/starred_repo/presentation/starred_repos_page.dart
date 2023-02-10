1cf7cfc9b71f88664f0487b56b1275e35445be9b
Bharat Soni <bharatchills@gmail.com>
2023-02-10 22:26:55 +0530

    auth providers and some stuff

---
+++ lib/starred_repo/presentation/starred_repos_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+
+class StarredReposPage extends StatelessWidget {
+  const StarredReposPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
