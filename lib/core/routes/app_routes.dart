b9912e1187849ce4763c5646bf7400115250eff3
Bharat Soni <bharatchills@gmail.com>
2023-02-12 23:13:56 +0530

    shared domain layer of app

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -2,7 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:repo_viewer/auth/presentation/authorization_page.dart';
 import 'package:repo_viewer/auth/presentation/sign_in_page.dart';
 import 'package:repo_viewer/splash/splash_page.dart';
-import 'package:repo_viewer/starred_repo/presentation/starred_repos_page.dart';
+import 'package:repo_viewer/github/repos/starred_repo/presentation/starred_repos_page.dart';
 
 @MaterialAutoRouter(
   routes: [
